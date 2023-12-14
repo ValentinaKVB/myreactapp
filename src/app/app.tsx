dfc425a8ec512f4a17df60a7fa9091a2bd0f147d
ValentinaKVB <rdirf@yandex.ru>
2023-12-14 19:31:50 +0300

    Создать приложение node, настроить прокси

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+export default function Home() {
 
-export function App() {
+  const url = '/aldis/promiseTest';
+  const [msg, setMsg] = useState('');
+
+  function handleClick() {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => setMsg(data.message))
+      .catch((error) => setMsg(error.message));
+  }
+
   return (
-    <div>
-      <NxWelcome title="myreactapp" />
+    <div className={styles.content}>
+      <div className={styles.container}>
+        <div className={styles.message}>{msg}</div>
+        <button className={styles.btnGet} onClick={handleClick}>Get Message</button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
